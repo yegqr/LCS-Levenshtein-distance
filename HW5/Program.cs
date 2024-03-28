a6c2dfd9cb0fac03cdf6c7161f1b750603454d3d
zuk <yisakov@kse.org.ua>
2024-03-28 15:25:46 +0200

    Initial commit

---
+++ HW5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using static HW5.WorkingWithInput;
+
+class Program
+{
+    private static void Main()
+    {
+        var path = "C:\\Users\\iskos\\RiderProjects\\Longest-common-subsequence-Levenshtein-distance\\HW5\\words_list.txt";
+        var wordsToWorkWith = FindWrongs(Console.ReadLine().ToLower() , CreateDict(path , new HashSet<string>()));
+        Console.WriteLine($"Looks like you have typos in next words: \"{string.Join("\" \"" , wordsToWorkWith)}\"");
+
+
+        var result = new Dictionary<string, PriorityQueue<Tuple<string , int> , int >>()  ;
+        foreach (var word in wordsToWorkWith)
+        {
+            result[word] = new PriorityQueue<Tuple<string , int>, int>(); 
+            result[word].Enqueue(Tuple.Create("0" , 0), 0 );
+            foreach (var fileWord in CreateDict(path , new HashSet<string>()))
+            {
+                if (fileWord.Length != word.Length )
+                {
+                    continue ;
+                }
+                var number = LCS(word, fileWord) ;
+                
+                if (result[word].Peek().Item2 < number)
+                {
+                    result[word].Enqueue(Tuple.Create(fileWord , number) , number);
+                    if (result[word].Count == 6)
+                    {
+                        result[word].Dequeue();
+                    }
+                }
+            }
+        }
+        
+        Console.WriteLine(1);
+
+          
+    }
+}
---
+++ HW5/WorkingWithInput.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+
+namespace HW5;
+
+public class WorkingWithInput
+{
+    public static HashSet<string> CreateDict(string path , HashSet<string> result)
+    {
+        foreach (var text in File.ReadAllLines(path))
+        {
+            result.Add(text.ToLower());
+        }
+        return result;
+    }
+
+    public static List<string> FindWrongs(string input, HashSet<string> checker )
+    {
+        var result = new List<string>();
+        var temporary = new StringBuilder();
+            
+        foreach (var character in input)
+        {
+            var x = Convert.ToInt32(character) ;
+            if (x is > 64 and < 91 or > 96 and < 123 )
+            {
+                temporary.Append(character);
+            }
+            else if (!checker.TryGetValue(temporary.ToString() , out input))
+            {
+                if (temporary.Length == 0)
+                {
+                    continue;
+                }
+                result.Add(temporary.ToString());
+                temporary.Clear();
+            }
+            else
+            {
+                temporary.Clear();
+            }
+        }
+        return result ;
+    }
+
+    public static int LCS(string word1, string word2)
+    {
+        var LCS =  new int[word1.Length + 1 , word2.Length + 1 ];
+
+        LCS[0, 0] = 0;
+        for (int i = 0; i <= word1.Length; i++)
+            LCS[i, 0] = 0;
+        for (int j = 0; j <= word2.Length; j++)
+            LCS[0, j] = 0;
+        
+        for (int i = 1; i < word1.Length +1 ; i++)
+        {
+            for (int j = 1; j < word2.Length +1 ; j++)
+            {
+                if (word1[i - 1] == word2[j - 1])
+                {
+                    LCS[i, j] = LCS[i - 1, j - 1] + 1; 
+                }
+                else
+                {
+                    LCS[i , j] = Math.Max(LCS[i - 1 ,j], LCS[i ,j - 1]);
+                }
+            }
+        }
+
+        return LCS[word1.Length, word2.Length];
+    }
+}
