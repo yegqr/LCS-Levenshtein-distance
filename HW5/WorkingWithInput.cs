00df4011e1e7e28cd2d0f9b40ab81327139a16bb
zuk <yisakov@kse.org.ua>
2024-03-30 13:06:11 +0200

    Final0.1

--- HW5/WorkingWithInput.cs
+++ HW5/WorkingWithInput.cs
@@ -4,33 +4,35 @@ namespace HW5;
 
 public class WorkingWithInput
 {
-    public static HashSet<string> CreateDict(string path , HashSet<string> result)
+    public static HashSet<string> CreateDict(string path, HashSet<string> result)
     {
         foreach (var text in File.ReadAllLines(path))
         {
             result.Add(text.ToLower());
         }
+
         return result;
     }
 
-    public static List<string> FindWrongs(string input, HashSet<string> checker )
+    public static List<string> FindWrongs(string input, HashSet<string> checker)
     {
         var result = new List<string>();
         var temporary = new StringBuilder();
-            
+
         foreach (var character in input)
         {
-            var x = Convert.ToInt32(character) ;
-            if (x is > 64 and < 91 or > 96 and < 123 )
+            var x = Convert.ToInt32(character);
+            if (x is > 64 and < 91 or > 96 and < 123)
             {
                 temporary.Append(character);
             }
-            else if (!checker.TryGetValue(temporary.ToString() , out input))
+            else if (!checker.TryGetValue(temporary.ToString(), out input))
             {
                 if (temporary.Length == 0)
                 {
                     continue;
                 }
+
                 result.Add(temporary.ToString());
                 temporary.Clear();
             }
@@ -39,34 +41,35 @@ public class WorkingWithInput
                 temporary.Clear();
             }
         }
-        return result ;
+
+        return result;
     }
 
     public static int LD(string word1, string word2)
     {
-        var LD =  new int[word1.Length + 1 , word2.Length + 1 ];
+        var LD = new int[word1.Length + 1, word2.Length + 1];
 
-        LD[0, 0] = 0;
         for (int i = 0; i <= word1.Length; i++)
             LD[i, 0] = i;
         for (int i = 0; i <= word2.Length; i++)
             LD[0, i] = i;
-        
-        for (int i = 1; i < word1.Length +1 ; i++)
+
+        for (int i = 1; i < word1.Length + 1; i++)
         {
-            for (int j = 1; j < word2.Length +1 ; j++)
+            for (int j = 1; j <= word2.Length; j++)
             {
-                if (word1[i - 1] == word2[j - 1])
+                int cost = (word1[i - 1] == word2[j - 1]) ? 0 : 1;
+
+                LD[i, j] = Math.Min(
+                    Math.Min(LD[i - 1, j] + 1, LD[i, j - 1] + 1),
+                    LD[i - 1, j - 1] + cost);
+                
+                if (i > 1 && j > 1 && word1[i - 1] == word2[j - 2] && word1[i - 2] == word2[j - 1])
                 {
-                    LD[i, j] = Math.Min(LD[i - 1, j - 1], Math.Min(LD[i, j - 1] + 1 , LD[i - 1, j] + 1));
-                }
-                else
-                {
-                    LD[i, j] = Math.Min(LD[i - 1, j - 1] + 1, Math.Min(LD[i, j - 1] + 1 , LD[i - 1, j] + 1));
+                    LD[i, j] = Math.Min(LD[i, j], LD[i - 2, j - 2] + cost);
                 }
             }
         }
-
         return LD[word1.Length, word2.Length];
     }
 }
--- HW5/obj/Debug/net8.0/HW5.AssemblyInfo.cs
+++ HW5/obj/Debug/net8.0/HW5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HW5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6c2dfd9cb0fac03cdf6c7161f1b750603454d3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc56f35bf74f354128a4c0ddb30c4d97827cac97")]
 [assembly: System.Reflection.AssemblyProductAttribute("HW5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HW5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
